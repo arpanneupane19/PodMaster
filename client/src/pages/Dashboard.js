b16a6794fd4417bcfe60255e30bbecff4da72643
Arpan Neupane <arpanneupane19@gmail.com>
2021-12-28 16:06:37 -0600

    added stuff to dashboard page

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -27,7 +27,13 @@ function Dashboard() {
   return (
     <div className="font-sans antialiased bg-white">
       <LoggedInNavbar />
-      <h1>Dashboard</h1>
+      <div className="flex flex-col md:pt-36 pt-24 mx-8">
+        <div className="flex flex-col">
+          <div className="mb-8 flex flex-col">
+            <h1 className="text-2xl mb-2 tracking-widest">Dashboard</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
