2fbc54e5f86758c988b5d2144c68d268bf98af1b
Arpan Neupane <arpanneupane19@gmail.com>
2022-01-19 23:56:24 -0600

    minor updates to frontend

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import { LoggedInNavbar } from "../components/Navbar";
 import axios from "axios";
+import {
+  AiOutlineHeart,
+  AiFillHeart,
+  AiOutlineComment,
+  AiOutlineEdit,
+} from "react-icons/ai";
 
 function Dashboard() {
   const [loggedIn, setLoggedIn] = useState(true);
@@ -39,29 +45,40 @@ function Dashboard() {
             <h1 className="text-2xl mb-2 tracking-widest">DASHBOARD</h1>
           </div>
           <p className="text-xl mb-2 tracking-widest">My Podcasts</p>
-          <div className="md:flex md:justify-center">
+          <div>
             {typeof podcasts !== "undefined" ? (
               podcasts.length > 0 ? (
                 podcasts.map((podcast, index) => (
-                  <div
-                    key={index}
-                    className="podcast md:w-1/2 md:my-4 md:ml-1 md:mr-1 w-full my-2 p-6 shadow-xl rounded-xl bg-red-500 text-white"
-                  >
-                    <div className="flex flex-col">
-                      <h3 className="font-bold text-xl mb-2">
-                        {podcast.podcast_title}
-                      </h3>
-                      <div className="h-24 overflow-auto mb-2">
-                        {podcast.podcast_description}
-                      </div>
-                      <div className="flex justify-between items-center">
-                        <div className="flex flex-col text-left">
-                          <span>{podcast.likes} likes</span>
-                          <p>Like</p>
+                  <div className="md:flex md:justify-center">
+                    <div
+                      key={index}
+                      className="podcast md:w-1/2 md:my-4 md:ml-1 md:mr-1 w-full my-2 p-6 shadow-xl rounded-xl bg-red-500 text-white"
+                    >
+                      <div className="flex flex-col">
+                        <div className="flex justify-between">
+                          <h3 className="flex font-bold text-xl mb-2">
+                            {podcast.podcast_title}
+                          </h3>
+                          <AiOutlineEdit className="text-2xl cursor-pointer" />
                         </div>
-                        <div className="flex flex-col text-right">
-                          <span>{podcast.comments} comments</span>
-                          <p>Comment</p>
+                        <div className="h-24 overflow-auto mb-2">
+                          {podcast.podcast_description}
+                        </div>
+                        <div className="flex justify-between items-center mb-2">
+                          {podcast.likes === 1 ? (
+                            <span>{podcast.likes} like</span>
+                          ) : (
+                            <span>{podcast.likes} likes</span>
+                          )}
+                          {podcast.comments === 1 ? (
+                            <span>{podcast.comments} comment</span>
+                          ) : (
+                            <span>{podcast.comments} comments</span>
+                          )}
+                        </div>
+                        <div className="flex justify-between items-center">
+                          <AiOutlineHeart className="text-2xl cursor-pointer" />
+                          <AiOutlineComment className="text-2xl cursor-pointer" />
                         </div>
                       </div>
                     </div>
