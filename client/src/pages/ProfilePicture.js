b95d10c8e573c037b27092f7832b0887ec09be09
Arpan Neupane <arpanneupane19@gmail.com>
2022-01-22 00:16:15 -0600

    added loading animation

--- client/src/pages/ProfilePicture.js
+++ client/src/pages/ProfilePicture.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import axios from "axios";
 import { LoggedInNavbar } from "../components/Navbar";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function ProfilePicture() {
   const [loggedIn, setLoggedIn] = useState(true);
   const [profilePicture, setProfilePicture] = useState(null);
   const [profilePicResponse, setProfilePicResponse] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios
@@ -21,6 +23,7 @@ function ProfilePicture() {
           localStorage.removeItem("token");
         }
       });
+    setLoading(false);
   }, []);
 
   if (!localStorage.getItem("token") || !loggedIn) {
@@ -53,49 +56,64 @@ function ProfilePicture() {
       });
   };
 
-  return (
-    <div className="font-sans antialiased bg-white">
-      <LoggedInNavbar />
-      <div className="flex flex-col justify-center items-center md:pt-36 pt-24 mx-6">
-        <div className="form md:w-3/4 w-full">
-          <div className="tracking-wider mb-8">
-            <h1 className="text-2xl tracking-widest mb-2">
-              Update Profile Picture
-            </h1>
-            <p>{profilePicResponse}</p>
-            {profilePicResponse ===
-            "Your profile picture has been uploaded successfully!" ? (
-              <a href="/account" className="text-blue-500">
-                Back to my account
-              </a>
-            ) : (
-              <></>
-            )}
-          </div>
-          <form onSubmit={updateProfilePicture} encType="mutlipart/form-data">
-            <div className="w-full flex justify-center items-center">
-              <input
-                type="file"
-                placeholder="Profile Picture"
-                id="file"
-                name="file"
-                onChange={(e) => setProfilePicture(e.target.files[0])}
-                className="w-full p-4 border border-gray-100 bg-gray-100 rounded-xl focus:border-gray-100 focus:outline-none focus:bg-white tracking-wide mb-4"
-                accept="image/*"
-                required
-              />
+  if (loading) {
+    return (
+      <div
+        style={{
+          position: "absolute",
+          left: "50%",
+          top: "45%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <ClipLoader color="#EF4444" size={100} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="font-sans antialiased bg-white">
+        <LoggedInNavbar />
+        <div className="flex flex-col justify-center items-center md:pt-36 pt-24 mx-6">
+          <div className="form md:w-3/4 w-full">
+            <div className="tracking-wider mb-8">
+              <h1 className="text-2xl tracking-widest mb-2">
+                Update Profile Picture
+              </h1>
+              <p>{profilePicResponse}</p>
+              {profilePicResponse ===
+              "Your profile picture has been uploaded successfully!" ? (
+                <a href="/account" className="text-blue-500">
+                  Back to my account
+                </a>
+              ) : (
+                <></>
+              )}
             </div>
-            <button
-              type="submit"
-              className="p-4 bg-red-500 text-white rounded-xl"
-            >
-              Update Profile Picture
-            </button>
-          </form>
+            <form onSubmit={updateProfilePicture} encType="mutlipart/form-data">
+              <div className="w-full flex justify-center items-center">
+                <input
+                  type="file"
+                  placeholder="Profile Picture"
+                  id="file"
+                  name="file"
+                  onChange={(e) => setProfilePicture(e.target.files[0])}
+                  className="w-full p-4 border border-gray-100 bg-gray-100 rounded-xl focus:border-gray-100 focus:outline-none focus:bg-white tracking-wide mb-4"
+                  accept="image/*"
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="p-4 bg-red-500 text-white rounded-xl"
+              >
+                Update Profile Picture
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ProfilePicture;
