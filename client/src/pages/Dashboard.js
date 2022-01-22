78959fdbd55e7ae9b121c7576e8f096ee80302bf
Arpan Neupane <arpanneupane19@gmail.com>
2022-01-22 00:15:31 -0600

    minor changes, added loading animation, and link to view user profile

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -8,10 +8,13 @@ import {
   AiOutlineComment,
   AiOutlineEdit,
 } from "react-icons/ai";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function Dashboard() {
   const [loggedIn, setLoggedIn] = useState(true);
+  const [loading, setLoading] = useState(true);
   const [podcasts, setPodcasts] = useState([{}]);
+  const [currentUserUsername, setCurrentUserUsername] = useState("");
 
   useEffect(() => {
     axios
@@ -28,79 +31,104 @@ function Dashboard() {
 
         if (response.data.message === "Verification successful.") {
           setPodcasts(response.data.podcasts);
+          setCurrentUserUsername(response.data.currentUserUsername);
         }
       });
+    setLoading(false);
   }, []);
 
   if (!localStorage.getItem("token") || !loggedIn) {
     return <Redirect to="/login" />;
   }
 
-  return (
-    <div className="font-sans antialiased bg-white">
-      <LoggedInNavbar />
-      <div className="flex flex-col md:pt-36 pt-24 mx-8">
-        <div className="flex flex-col">
-          <div className="mb-8 flex flex-col">
-            <h1 className="text-2xl mb-2 tracking-widest">DASHBOARD</h1>
-          </div>
-          <p className="text-xl mb-2 tracking-widest">My Podcasts</p>
-          <div>
-            {typeof podcasts !== "undefined" ? (
-              podcasts.length > 0 ? (
-                podcasts.map((podcast, index) => (
-                  <div className="md:flex md:justify-center">
-                    <div
-                      key={index}
-                      className="podcast md:w-1/2 md:my-4 md:ml-1 md:mr-1 w-full my-2 p-6 shadow-xl rounded-xl bg-red-500 text-white"
-                    >
-                      <div className="flex flex-col">
-                        <div className="flex justify-between">
-                          <h3 className="flex font-bold text-xl mb-2">
-                            {podcast.podcast_title}
-                          </h3>
-                          <Link
-                            to={{
-                              pathname: `/edit-podcast/${podcast.podcast_id}`,
-                            }}
-                          >
-                            <AiOutlineEdit className="text-2xl cursor-pointer" />
-                          </Link>
-                        </div>
-                        <div className="h-24 overflow-auto mb-2">
-                          {podcast.podcast_description}
-                        </div>
-                        <div className="flex justify-between items-center mb-2">
-                          {podcast.likes === 1 ? (
-                            <span>{podcast.likes} like</span>
-                          ) : (
-                            <span>{podcast.likes} likes</span>
-                          )}
-                          {podcast.comments === 1 ? (
-                            <span>{podcast.comments} comment</span>
-                          ) : (
-                            <span>{podcast.comments} comments</span>
-                          )}
-                        </div>
-                        <div className="flex justify-between items-center">
-                          <AiOutlineHeart className="text-2xl cursor-pointer" />
-                          <AiOutlineComment className="text-2xl cursor-pointer" />
+  if (loading) {
+    return (
+      <div
+        style={{
+          position: "absolute",
+          left: "50%",
+          top: "45%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <ClipLoader color="#EF4444" size={100} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="font-sans antialiased bg-white">
+        <LoggedInNavbar />
+        <div className="flex flex-col md:pt-36 pt-24 mx-8">
+          <div className="flex flex-col">
+            <div className="mb-8 flex flex-col">
+              <h1 className="text-2xl mb-2 tracking-widest">My Podcasts</h1>
+              <span className="tracking-wider">
+                View my{" "}
+                <Link
+                  className="underline underline-offset-1 text-blue-500"
+                  to={{ pathname: `/user/${currentUserUsername}` }}
+                >
+                  profile
+                </Link>
+              </span>
+            </div>
+            <div>
+              {typeof podcasts !== "undefined" ? (
+                podcasts.length > 0 ? (
+                  podcasts.map((podcast, index) => (
+                    <div className="md:flex md:justify-center">
+                      <div
+                        key={index}
+                        className="podcast md:w-1/2 md:my-4 md:ml-1 md:mr-1 w-full my-2 p-6 shadow-xl rounded-xl bg-red-500 text-white"
+                      >
+                        <div className="flex flex-col">
+                          <div className="flex justify-between">
+                            <h3 className="flex font-bold text-xl mb-2">
+                              {podcast.podcast_title}
+                            </h3>
+                            <Link
+                              to={{
+                                pathname: `/edit-podcast/${podcast.podcast_id}`,
+                              }}
+                            >
+                              <AiOutlineEdit className="text-2xl cursor-pointer" />
+                            </Link>
+                          </div>
+                          <div className="h-24 overflow-auto mb-2">
+                            {podcast.podcast_description}
+                          </div>
+                          <div className="flex justify-between items-center mb-2">
+                            {podcast.likes === 1 ? (
+                              <span>{podcast.likes} like</span>
+                            ) : (
+                              <span>{podcast.likes} likes</span>
+                            )}
+                            {podcast.comments === 1 ? (
+                              <span>{podcast.comments} comment</span>
+                            ) : (
+                              <span>{podcast.comments} comments</span>
+                            )}
+                          </div>
+                          <div className="flex justify-between items-center">
+                            <AiOutlineHeart className="text-2xl cursor-pointer" />
+                            <AiOutlineComment className="text-2xl cursor-pointer" />
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                ))
+                  ))
+                ) : (
+                  <p>You have no podcasts at the moment.</p>
+                )
               ) : (
-                <p>You have no podcasts at the moment.</p>
-              )
-            ) : (
-              <p>Loading...</p>
-            )}
+                <p>Loading...</p>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Dashboard;
