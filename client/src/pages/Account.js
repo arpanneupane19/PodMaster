9e20d4d4270560c57fdff0a2cc8657242a105ec8
Arpan Neupane <arpanneupane19@gmail.com>
2022-01-22 00:14:51 -0600

    added loading animation

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, Redirect } from "react-router-dom";
 import { LoggedInNavbar } from "../components/Navbar";
 import axios from "axios";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function Dashboard() {
   const [loggedIn, setLoggedIn] = useState(true);
@@ -12,6 +13,7 @@ function Dashboard() {
   const [imgUrl, setImgUrl] = useState("");
   const [alreadyExists, setAlreadyExists] = useState(false);
   const [accountUpdated, setAccountUpdated] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios
@@ -41,6 +43,7 @@ function Dashboard() {
           }
         }
       });
+    setLoading(false);
   }, []);
 
   const updateAccount = (event) => {
@@ -88,123 +91,137 @@ function Dashboard() {
   if (!localStorage.getItem("token") || !loggedIn) {
     return <Redirect to="/login" />;
   }
+  if (loading) {
+    return (
+      <div
+        style={{
+          position: "absolute",
+          left: "50%",
+          top: "45%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <ClipLoader color="#EF4444" size={100} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="font-sans antialiased bg-white">
+        <LoggedInNavbar />
+        <div className="flex flex-col justify-center items-center md:pt-36 pt-24 mx-6">
+          <div className="form md:w-3/4 w-full">
+            <div className="tracking-wider mb-6">
+              <h1 className="text-2xl tracking-widest mb-2">My Account</h1>
+              <img
+                src={imgUrl}
+                alt="Profile"
+                className="rounded-full mt-4 mb-4"
+                width="100"
+              />
+              {alreadyExists ? (
+                <>
+                  <span>
+                    Username or email already exists. Please choose a different
+                    one.
+                  </span>
+                </>
+              ) : (
+                <></>
+              )}
 
-  return (
-    <div className="font-sans antialiased bg-white">
-      <LoggedInNavbar />
-      <div className="flex flex-col justify-center items-center md:pt-36 pt-24 mx-6">
-        <div className="form md:w-3/4 w-full">
-          <div className="tracking-wider mb-6">
-            <h1 className="text-2xl tracking-widest mb-2">My Account</h1>
-            <img
-              src={imgUrl}
-              alt="Profile"
-              className="rounded-full mt-4 mb-4"
-              width="100"
-            />
-            {alreadyExists ? (
-              <>
-                <span>
-                  Username or email already exists. Please choose a different
-                  one.
-                </span>
-              </>
-            ) : (
-              <></>
-            )}
-
-            {accountUpdated ? (
-              <>
-                <span>Your account has successfully been updated!</span>
-              </>
-            ) : (
-              <></>
-            )}
-          </div>
-          <form onSubmit={updateAccount}>
-            <div className="w-full flex justify-center items-center">
+              {accountUpdated ? (
+                <>
+                  <span>Your account has successfully been updated!</span>
+                </>
+              ) : (
+                <></>
+              )}
+            </div>
+            <form onSubmit={updateAccount}>
+              <div className="w-full flex justify-center items-center">
+                <input
+                  type="text"
+                  min="4"
+                  max="20"
+                  placeholder="First Name"
+                  name="username"
+                  id="username"
+                  onChange={(e) => setFirstName(e.target.value)}
+                  value={firstName}
+                  className="w-1/2 mr-1 p-4 border-b-2 focus:border-gray-100 outline-none tracking-wide mb-4"
+                  required
+                />
+                <input
+                  type="text"
+                  min="4"
+                  max="30"
+                  placeholder="Last Name"
+                  name="username"
+                  id="username"
+                  onChange={(e) => setLastName(e.target.value)}
+                  value={lastName}
+                  className="w-1/2 ml-1 p-4 border-b-2 focus:border-gray-100 outline-none tracking-wide mb-4"
+                  required
+                />
+              </div>
               <input
                 type="text"
                 min="4"
                 max="20"
-                placeholder="First Name"
+                placeholder="Username"
                 name="username"
                 id="username"
-                onChange={(e) => setFirstName(e.target.value)}
-                value={firstName}
-                className="w-1/2 mr-1 p-4 border-b-2 focus:border-gray-100 outline-none tracking-wide mb-4"
+                onChange={(e) => setUsername(e.target.value.toLowerCase())}
+                value={username}
+                className="w-full p-4 border-b-2 focus:border-gray-100 outline-none tracking-wide mb-4"
                 required
               />
               <input
-                type="text"
-                min="4"
-                max="30"
-                placeholder="Last Name"
-                name="username"
-                id="username"
-                onChange={(e) => setLastName(e.target.value)}
-                value={lastName}
-                className="w-1/2 ml-1 p-4 border-b-2 focus:border-gray-100 outline-none tracking-wide mb-4"
+                type="email"
+                min="7"
+                max="320"
+                placeholder="Email"
+                name="email"
+                id="email"
+                onChange={(e) => setEmail(e.target.value.toLowerCase())}
+                value={email}
+                className="w-full p-4 border-b-2 focus:border-gray-100 outline-none tracking-wide mb-8"
                 required
               />
-            </div>
-            <input
-              type="text"
-              min="4"
-              max="20"
-              placeholder="Username"
-              name="username"
-              id="username"
-              onChange={(e) => setUsername(e.target.value.toLowerCase())}
-              value={username}
-              className="w-full p-4 border-b-2 focus:border-gray-100 outline-none tracking-wide mb-4"
-              required
-            />
-            <input
-              type="email"
-              min="7"
-              max="320"
-              placeholder="Email"
-              name="email"
-              id="email"
-              onChange={(e) => setEmail(e.target.value.toLowerCase())}
-              value={email}
-              className="w-full p-4 border-b-2 focus:border-gray-100 outline-none tracking-wide mb-8"
-              required
-            />
-            <div className="flex flex-col items-start mb-4">
-              <span>
-                Want to update your profile picture? Click{" "}
-                <Link
-                  to="/update-profile-picture"
-                  className="underline hover:no-underline"
-                >
-                  here
-                </Link>
-                .
-              </span>
-              <span>
-                Want to change your password? Click{" "}
-                <Link
-                  className="underline hover:no-underline"
-                  to="/change-password"
-                >
-                  here
-                </Link>
-                .
-              </span>
-            </div>
-            <button
-              type="submit"
-              className="p-4 bg-red-500 text-white rounded-xl"
-            >
-              Update Account
-            </button>
-          </form>
+              <div className="flex flex-col items-start mb-4">
+                <span>
+                  Want to update your profile picture? Click{" "}
+                  <Link
+                    to="/update-profile-picture"
+                    className="underline hover:no-underline"
+                  >
+                    here
+                  </Link>
+                  .
+                </span>
+                <span>
+                  Want to change your password? Click{" "}
+                  <Link
+                    className="underline hover:no-underline"
+                    to="/change-password"
+                  >
+                    here
+                  </Link>
+                  .
+                </span>
+              </div>
+              <button
+                type="submit"
+                className="p-4 bg-red-500 text-white rounded-xl"
+              >
+                Update Account
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Dashboard;
