5289a949132956b45bad8fe7c65d59e0c8c22a1f
Arpan Neupane <arpanneupane19@gmail.com>
2022-02-11 20:12:50 -0600

    added page to view comments and deleting comments also works

--- client/src/pages/Comments.js
+++ client/src/pages/Comments.js
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from "react";
 import { Redirect, useParams, Link } from "react-router-dom";
 import { LoggedInNavbar } from "../components/Navbar";
 import axios from "axios";
+import { FiTrash2 } from "react-icons/fi";
 import ClipLoader from "react-spinners/ClipLoader";
 
 function Comments() {
   const [loggedIn, setLoggedIn] = useState(true);
   const [notFound, setNotFound] = useState(false);
+  const [commentNotFound, setCommentNotFound] = useState(false);
+  const [commentDeleted, setCommentDeleted] = useState(false);
+  const [forbidden, setForbidden] = useState(false);
   const [loading, setLoading] = useState(true);
   const [podcastTitle, setPodcastTitle] = useState("");
   const [podcastOwnerUsername, setPodcastOwnerUsername] = useState("");
+  const [currentUserUsername, setCurrentUserUsername] = useState("");
   const [comments, setComments] = useState([{}]);
 
   const { podcastId } = useParams();
@@ -31,6 +36,7 @@ function Comments() {
           if (response.data.podcastExists) {
             setPodcastTitle(response.data.podcastTitle);
             setPodcastOwnerUsername(response.data.podcastOwnerUsername);
+            setCurrentUserUsername(response.data.currentUserUsername);
             setComments(response.data.comments);
           } else {
             setNotFound(true);
@@ -38,7 +44,29 @@ function Comments() {
         }
       });
     setLoading(false);
-  }, [podcastId]);
+  }, [podcastId, commentDeleted]);
+
+  const deleteComment = (id) => {
+    axios
+      .post(`/api/delete-comment/${id}`, id, {
+        headers: {
+          "x-access-token": localStorage.getItem("token"),
+        },
+      })
+      .then((response) => {
+        if (response.data.commentExists) {
+          if (response.data.commentOwnerValid) {
+            if (response.data.commentDeleted) {
+              setCommentDeleted(true);
+            }
+          } else {
+            setForbidden(true);
+          }
+        } else {
+          setCommentNotFound(true);
+        }
+      });
+  };
 
   if (!localStorage.getItem("token") || !loggedIn) {
     return <Redirect to="/login" />;
@@ -46,6 +74,14 @@ function Comments() {
 
   if (notFound) {
     return <Redirect to="/podcast-not-found" />;
+  }
+
+  if (forbidden) {
+    return <Redirect to="/403-forbidden" />;
+  }
+
+  if (commentNotFound) {
+    return <Redirect to="/comment-not-found" />;
   }
 
   if (loading) {
@@ -91,13 +127,35 @@ function Comments() {
                     >
                       <div className="flex flex-col">
                         <div className="commenter-username">
-                          @
-                          <Link
-                            className="underline tracking-wide"
-                            to={{ pathname: `/user/${comment.commenter}` }}
-                          >
-                            {comment.commenter}
-                          </Link>
+                          {comment.commenter === currentUserUsername ? (
+                            <div className="flex justify-between">
+                              <div>
+                                @
+                                <Link
+                                  className="underline tracking-wide"
+                                  to={{
+                                    pathname: `/user/${comment.commenter}`,
+                                  }}
+                                >
+                                  {comment.commenter}
+                                </Link>
+                              </div>
+                              <FiTrash2
+                                onClick={() => deleteComment(comment.commentId)}
+                                className="text-xl cursor-pointer ml-2"
+                              />
+                            </div>
+                          ) : (
+                            <>
+                              @
+                              <Link
+                                className="underline tracking-wide"
+                                to={{ pathname: `/user/${comment.commenter}` }}
+                              >
+                                {comment.commenter}
+                              </Link>
+                            </>
+                          )}
                         </div>
                         <div className="comment-body">
                           <p>{comment.comment}</p>
