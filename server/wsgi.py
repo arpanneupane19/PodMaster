8994699a6b4aa8e4b5f23e1af754e82c50e6228e
Arpan Neupane <arpanneupane19@gmail.com>
2022-04-15 18:47:43 -0500

    separate file for running app and added copyright

---
+++ server/wsgi.py
@@ -0,0 +1,10 @@
+# Code written by Arpan Neupane.
+# Copyright (c) Arpan Neupane 2022. All rights reserved.
+
+from configs import *
+from models import *
+from routes import *
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True, port='8080')
